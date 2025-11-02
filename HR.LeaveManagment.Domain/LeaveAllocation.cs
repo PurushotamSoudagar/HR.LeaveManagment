459f7c467d7ce40aa10c1e254fd3b317a44521ba
PurushotamSoudagar <purushotamsoudagar100@gmail.com>
2025-11-02 12:00:36 +0530

    Creating a Structure of HR.LeaveManagment Project

---
+++ HR.LeaveManagment.Domain/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagment.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public int LeaveTypeId { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int Period { get; set; }
+    }
+}
